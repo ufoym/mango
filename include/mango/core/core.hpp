b990423da8f8a0913b54fe8353ddbb3ab2e5d4db
Ming <a@ufoym.com>
2022-02-23 00:05:22 +0800

    Remove useless code

--- include/mango/core/core.hpp
+++ include/mango/core/core.hpp
@@ -5,15 +5,8 @@
 #pragma once
 
 #include <mango/core/configure.hpp>
-#include <mango/core/half.hpp>
-#include <mango/core/atomic.hpp>
-#include <mango/core/bits.hpp>
 #include <mango/core/endian.hpp>
 #include <mango/core/pointer.hpp>
-#include <mango/core/compress.hpp>
-#include <mango/core/crc32.hpp>
-#include <mango/core/hash.hpp>
-#include <mango/core/aes.hpp>
 #include <mango/core/cpuinfo.hpp>
 #include <mango/core/system.hpp>
 #include <mango/core/exception.hpp>
@@ -23,4 +16,3 @@
 #include <mango/core/memory.hpp>
 #include <mango/core/string.hpp>
 #include <mango/core/thread.hpp>
-#include <mango/core/dynamic_library.hpp>
