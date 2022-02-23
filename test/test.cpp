894348d99be95f306b48d9b5b9ca7b1e4bffbb91
Ming <a@ufoym.com>
2022-02-24 00:21:54 +0800

    Add working test

--- test/test.cpp
+++ test/test.cpp
@@ -1,34 +1,47 @@
+#include <string>
+#include <vector>
 #include <jpeg.hpp>
+#include <opencv2/opencv.hpp>
 
-void print(const char* name, std::uint64_t load, std::uint64_t save)
-{
-    printf("%s", name);
+void print(const std::string name, std::uint64_t load, std::uint64_t save, float diff) {
+    printf("%-16s", name.c_str());
     printf("%7d.%d ms ", int(load / 1000), int(load % 1000) / 100);
     printf("%7d.%d ms ", int(save / 1000), int(save % 1000) / 100);
+    printf("%12f ", diff);
     printf("\n");
 }
 
+float get_diff(const std::string pathA, const std::string pathB) {
+    cv::Mat imgA = cv::imread(pathA);
+    cv::Mat imgB = cv::imread(pathB);
+    return cv::norm(imgA, imgB, cv::NORM_RELATIVE | cv::NORM_L1);
+}
 
-int main(int argc, const char* argv[])
-{
-    const char* default_filename = "../../test/images/jpeg444.jpg";
-    const char* filename = argc > 1 ? argv[1] : default_filename;
+int main() {
+    const std::string root = "../../test/images/";
+    const std::string tmp = "output-tmp.jpg";
+    const std::vector<std::string> filenames = {
+        "jpeg444.jpg",
+        "jpeg400jfif.jpg",
+        "jpeg420exif.jpg",
+        "jpeg422jfif.jpg",
+    };
 
     printf("%s\n", mango::getSystemInfo().c_str());
-    printf("----------------------------------------------\n");
-    printf("                load         save             \n");
-    printf("----------------------------------------------\n");
+    printf("-----------------------------------------------------------\n");
+    printf("    test-case          load         save         diff      \n");
+    printf("-----------------------------------------------------------\n");
 
-    std::uint64_t time0;
-    std::uint64_t time1;
-    std::uint64_t time2;
+    for (auto filename: filenames) {
+        auto path = root + filename;
 
-    time0 = mango::Time::us();
-    mango::image::Surface bitmap = mango_load_jpeg(filename);
+        std::uint64_t time0 = mango::Time::us();
+        mango::image::Surface bitmap = mango_load_jpeg(path.c_str());
 
-    time1 = mango::Time::us();
-    mango_save_jpeg("output-mango.jpg", bitmap);
+        std::uint64_t time1 = mango::Time::us();
+        mango_save_jpeg(tmp.c_str(), bitmap);
 
-    time2 = mango::Time::us();
-    print("mango:   ", time1 - time0, time2 - time1);
+        std::uint64_t time2 = mango::Time::us();
+        print(filename, time1 - time0, time2 - time1, get_diff(path, tmp));
+    }
 }
